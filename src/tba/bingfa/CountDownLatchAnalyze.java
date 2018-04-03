a97b434c877afa07809f6c7570ce4026f50d8b93
zhangdong8 <zhangdong8@jd.com>
2018-04-03 15:46:40 +0800

    update

--- src/tba/bingfa/CountDownLatchAnalyze.java
+++ src/tba/bingfa/CountDownLatchAnalyze.java
@@ -15,7 +15,7 @@ public class CountDownLatchAnalyze {
         new Thread(){
             public void run() {
                 try {
-                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行...");
                     Thread.sleep(3000);
                     System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
                     latch.countDown();
