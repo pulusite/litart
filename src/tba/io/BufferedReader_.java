ba28d9935485cda3b86bfe6a2db77ca344e6fee4
zhangdong8 <zhangdong8@jd.com>
2018-04-08 12:12:31 +0800

    update

--- src/tba/io/BufferedReader_.java
+++ src/tba/io/BufferedReader_.java
@@ -3,6 +3,7 @@ package tba.io;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by dongzhang on 5/21/17.
@@ -18,7 +19,19 @@ public class BufferedReader_ {
         in.close();
         return sb.toString();
     }
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String args[]) throws Exception{
+        char c;
+        // 使用 System.in 创建 BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("输入字符, 按下 'q' 键退出。");
+        // 读取字符
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        } while(c != 'q');
+
+
         System.out.println(System.getProperty("user.dir"));
         System.out.println(read(System.getProperty("user.dir")+"/src/cc/io/BufferedReader_.java"));
     }
