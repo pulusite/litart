3f3ebfec3a0ca14ce771271bc1205ca1c1466306
zhangdong8 <zhangdong8@jd.com>
2018-05-23 17:12:56 +0800

    update

---
+++ src/tba/jihe/LinkedList_.java
@@ -0,0 +1,12 @@
+package tba.jihe;
+
+import java.util.LinkedList;
+
+/**
+ * Created by zhangdong on 2018/5/23.
+ */
+public class LinkedList_ {
+    public static void main(String[] args) {
+        LinkedList linkedList=new LinkedList();
+    }
+}
