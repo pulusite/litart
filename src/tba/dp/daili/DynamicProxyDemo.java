1614a6009ea6a1a2a040ae29baba4a57d0654f23
zhangdong8 <zhangdong8@jd.com>
2018-05-23 18:02:57 +0800

    update

--- src/tba/dp/daili/DynamicProxyDemo.java
+++ src/tba/dp/daili/DynamicProxyDemo.java
@@ -6,7 +6,9 @@ package tba.dp.daili;
 public class DynamicProxyDemo {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
+
         MyInvocationHandler invocationHandler = new MyInvocationHandler(userService);
+
         UserService proxy = (UserService) invocationHandler.getProxy();
         proxy.add();
     }
