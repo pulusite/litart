823518423d74c89ca403fc462d5747f08f72d047
zhangdong191 <zhangdong8@jd.com>
2018-06-29 10:25:33 +0800

    update

---
+++ src/leetcode/E018_4Sum.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhangdong on 2018/6/27.
+ *
+ Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+ Note:
+
+ The solution set must not contain duplicate quadruplets.
+
+ Example:
+
+ Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+ A solution set is:
+ [
+ [-1,  0, 0, 1],
+ [-2, -1, 1, 2],
+ [-2,  0, 0, 2]
+ ]
+ */
+public class E018_4Sum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums==null || nums.length<4) return null;
+        List<List<Integer>> result=new ArrayList<>();
+
+
+        return result;
+    }
+}
--- src/leetcode/E055_Jump_Game.java
+++ src/leetcode/E055_Jump_Game.java
@@ -1,7 +1,27 @@
 package leetcode;
 
+import org.junit.Test;
+
 /**
  * Created by zhangdong on 1/11/18.
+ *
+ Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+ Each element in the array represents your maximum jump length at that position.
+
+ Determine if you are able to reach the last index.
+
+ Example 1:
+
+ Input: [2,3,1,1,4]
+ Output: true
+ Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ Example 2:
+
+ Input: [3,2,1,0,4]
+ Output: false
+ Explanation: You will always arrive at index 3 no matter what. Its maximum
+ jump length is 0, which makes it impossible to reach the last index.
  */
 public class E055_Jump_Game {
     public boolean canJump(int[] nums) {
@@ -9,5 +29,11 @@ public class E055_Jump_Game {
         for (int reach = 0; i < nums.length && i <= reach; ++i)
             reach = Math.max(i + nums[i], reach);
         return i == nums.length;
+    }
+
+    @Test
+    public void test(){
+        int[] nums={2,3,1,1,4};
+        System.out.println(canJump(nums));
     }
 }
